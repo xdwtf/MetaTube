0a5f629d367f7b0aba53bdc1a947dff6293d6d5e
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-01-29 21:35:38 +0530

    Update metatube.py

--- metatube.py
+++ metatube.py
@@ -7,8 +7,8 @@ from distutils.util import strtobool
 import os
 if __name__ == "__main__":
     app = create_app()
-    port = os.environ.get('PORT)
-    host = os.environ.get('HOST)
+    port = os.environ.get('PORT')
+    host = os.environ.get('HOST')
     debug = os.environ.get('DEBUG', False)
     log_output = os.environ.get('LOG', True)
     get_hub().NOT_ERROR += (KeyboardInterrupt,)
