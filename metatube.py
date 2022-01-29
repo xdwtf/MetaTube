d5163c2c79be73de27aabd9e2611b0b0e9dedf4e
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-01-29 22:45:11 +0530

    Update metatube.py

--- metatube.py
+++ metatube.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     log_output = os.environ.get('LOG', True)
     get_hub().NOT_ERROR += (KeyboardInterrupt,)
     try:
-        print(u'Starting the webserver on http://%s:%s...'%(domain, port))
-        socketio.run(app, domain=domain, port=port, log_output=strtobool(str(log_output)))
+        #print(u'Starting the webserver on http://%s:%s...'%(domain, port))
+        socketio.run(app, port=port, log_output=strtobool(str(log_output)))
     except KeyboardInterrupt:
         print('Stopped server because of KeyboardInterrupt')
