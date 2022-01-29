968af6229911164d992138ef85a5a71584ad563c
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-01-29 21:26:39 +0530

    Update metatube.py

--- metatube.py
+++ metatube.py
@@ -7,10 +7,10 @@ from distutils.util import strtobool
 import os
 if __name__ == "__main__":
     app = create_app()
-    port = os.environ.get('PORT', 5000)
-    host = os.environ.get('HOST', '0.0.0.0')
+    port = os.environ.get('PORT)
+    host = os.environ.get('HOST)
     debug = os.environ.get('DEBUG', False)
-    log_output = os.environ.get('LOG', False)
+    log_output = os.environ.get('LOG', True)
     get_hub().NOT_ERROR += (KeyboardInterrupt,)
     try:
         print(u'Starting the webserver on http://%s:%s...'%(host, port))
