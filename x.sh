c383783de1c0f9e577cf7238614cdb5ba3c24ed4
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-01-29 22:15:32 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,1 +1,7 @@
+echo "git clone, Please Wait..."
+git clone https://github.com/xdwtf/Fheroku.git /metatube
+echo "Trying Requirements..."
+cd /metatube
+pip3 install -U -r requirements.txt
+echo "starting web"
 python3 metatube.py
